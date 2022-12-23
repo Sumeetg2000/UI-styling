9dc833809e3183dafd0a899d690788bd95f04c65
Sumeet <sumeet.g@simformsolutions.com>
2022-12-23 20:46:59 +0530

    demo site

---
+++ demo-site/src/components/Footer.tsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Techy</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="#" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Sumeetg2000" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
